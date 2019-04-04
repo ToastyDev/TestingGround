52aa4a9d7668f272b6ff89623da279d26ac415f2
Toasty-Laptop <curtisgauntdev@gmail.com>
2019-04-03 22:13:46 -0400

    Created floaty furniture classes updated ignore

---
+++ Source/TestingGround/FloatyFurniture.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FloatyFurniture.h"
+
+
+// Sets default values
+AFloatyFurniture::AFloatyFurniture()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AFloatyFurniture::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFloatyFurniture::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	FVector NewLocation = GetActorLocation();
+	if (NewLocation.Z <= 200)
+		NewLocation.Z += 20 * DeltaTime;
+	else if (NewLocation.Z >= 500)
+		NewLocation.Z -= 20 * DeltaTime;
+	SetActorLocation(NewLocation);
+
+}
+
